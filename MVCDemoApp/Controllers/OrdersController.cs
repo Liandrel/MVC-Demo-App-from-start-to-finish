a18c0f0c64241333656b613e0ce9b4e6f17b76b6
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-27 21:47:59 +0200

    OrdersController with Crete View
    Created Controller for orders with view for creation order model

---
+++ MVCDemoApp/Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCDemoApp.Models;
+
+namespace MVCDemoApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderData _orderData;
+        private readonly IFoodData _foodData;
+
+        public OrdersController(IOrderData orderData, IFoodData foodData)
+        {
+            _orderData = orderData;
+            _foodData = foodData;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var food = await _foodData.GetFood();
+
+            CreateOrderModel model = new();
+
+            food.ForEach(x =>
+            {
+                model.FoodItems.Add(new SelectListItem { Value = x.Id.ToString(), Text = x.Title });
+            });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderModel order)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return View();
+            }
+
+            var food = await  _foodData.GetFood();
+
+            order.Total = order.Quantity * food.Where(x => x.Id == order.FoodId).First().Price;
+
+            int id = await _orderData.CreateOrder(order);
+
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ MVCDemoApp/Models/CreateOrderModel.cs
@@ -0,0 +1,13 @@
+﻿using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVCDemoApp.Models
+{
+    public class CreateOrderModel
+    {
+        public OrderModel Order { get; set; } = new();
+
+        public List<SelectListItem> FoodItems { get; set; } = new();
+
+    }
+}
