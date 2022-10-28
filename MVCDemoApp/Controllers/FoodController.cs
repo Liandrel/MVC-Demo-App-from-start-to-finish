dde9d36eca1a0869819a68b116d7641e5a2d567d
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-28 18:56:34 +0200

    FoodController
    created food controller with index view representing all avaiable meals

---
+++ MVCDemoApp/Controllers/FoodController.cs
@@ -0,0 +1,25 @@
+﻿using DataLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace MVCDemoApp.Controllers
+{
+    public class FoodController : BaseController
+    {
+        private readonly IFoodData _foodData;
+
+        public FoodController(IFoodData foodData)
+        {
+            _foodData = foodData;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var food = await _foodData.GetFood();
+
+            
+
+            return View(food);
+        }
+    }
+}
